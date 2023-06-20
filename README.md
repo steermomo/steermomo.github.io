1ad2784c4843fe99a2798ad01b1d1d2386366e22
steermomo <10905619+steermomo@users.noreply.github.com>
2023-06-20 22:15:48 +0800

    update dependencies

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # i.steer.space
 
 ![GitHub Action Automatic Build](https://github.com/steermomo/steermomo.github.io/workflows/Python%20application/badge.svg?branch=origin)
+
+| desc | command |
+| --- | --- |
+| install poetry up| poetry self add poetry-plugin-up |
+| update each package to latest version |poetry show -o \| awk '{print $1}' \| xargs poetry update |
+
+
--- poetry_export.sh
+++ poetry_export.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-poetry export --without-urls --without-hashes -f requirements.txt --output requirements.txt
+TEMP_FILE=$(mktemp)
+echo "Temporary file: $TEMP_FILE"
+
+poetry export --without-urls --without-hashes -f requirements.txt --output "$TEMP_FILE"
+
+# read from TEMP_FILE, split by , and write first column to requirements.txt
+awk -F';' '{print $1}' "$TEMP_FILE" > requirements.txt
